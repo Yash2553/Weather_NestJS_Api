4adf4bf907fd9644c63ff3a9c101f91e96d9448e
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-15 12:22:02 +0530

    Add files via upload

---
+++ weather_api/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { WeatherModule } from './weather/weather.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({ uri: config.get('MONGO_URI') }),
+      inject: [ConfigService],
+    }),
+    WeatherModule,
+  
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
---
+++ weather_api/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
+ async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    ['/weather/add'],
+    basicAuth({
+      challenge: true,
+      // this is the username and password used to authenticate
+      users: { admin: 'password' },
+    }),
+  );  
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather')
+    .setDescription('Fetch weather for configured cities using Open Weather API.')
+    .setVersion('1.0')
+    .addTag('weather')
+    .addBasicAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
+
