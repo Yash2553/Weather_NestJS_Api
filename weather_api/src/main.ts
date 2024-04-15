2ba9ebf36fc29686a6679cce7733bba41ea522d7
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-15 16:10:54 +0530

    Update main.ts

--- weather_api/src/main.ts
+++ weather_api/src/main.ts
@@ -3,57 +3,28 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as basicAuth from 'express-basic-auth';
 
-async function bootstrap() {
+ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(
     ['/weather/add'],
     basicAuth({
       challenge: true,
+      // this is the username and password used to authenticate
       users: { admin: 'password' },
     }),
-  );
-
+  );  
 
   const config = new DocumentBuilder()
-    .setTitle('Weather API')
-    .setDescription('API to fetch weather for configured cities using Open Weather API.')
+    .setTitle('Weather')
+    .setDescription('Fetch weather for configured cities using Open Weather API.')
     .setVersion('1.0')
     .addTag('weather')
     .addBasicAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-
-  const options = {
-    swaggerOptions: {
-      schemas: [
-        {
-          CreateCityDTO: {
-            type: 'object',
-            properties: {
-              city: { type: 'string' },
-              latitude: { type: 'number' },
-              longitude: { type: 'number' },
-            },
-            required: ['city', 'latitude', 'longitude'],
-          },
-        },
-        {
-          WeatherResponse: {
-            type: 'array',
-            items: {
-              type: 'object',
-              properties: {
-                city: { type: 'string' },
-                weather: { type: 'string' },
-              },
-            },
-          },
-        },
-      ],
-    },
-  };
-  SwaggerModule.setup('api', app, document, options);
-
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
+
