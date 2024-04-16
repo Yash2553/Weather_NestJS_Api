1c13519ff8ed47337251b5ccad6ab8259c999d5c
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-16 12:57:27 +0530

    Update weather.controller.ts

--- weather_api/src/weather/weather.controller.ts
+++ weather_api/src/weather/weather.controller.ts
@@ -1,28 +1,38 @@
 import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateCityDTO } from './dtos/CreateCity.dto';
 import { WeatherService } from 'src/weather/weather.service';
-import { ApiBasicAuth, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { GetWeatherDTO } from './dtos/GetWeather.dto';
+import { GetAllCitiesDTO } from './dtos/GetAllCities.dto';
 
+
+@ApiTags('weather')
 @Controller('weather')
 export class WeatherController {
     constructor (private readonly weatherService: WeatherService, ) {}
-
+    
+    
+    @ApiOkResponse({type:GetWeatherDTO, description:"Weather of all configured cities"})
     @Get()
     getWeather() {
         return this.weatherService.getWeather();
     }
+ 
 
+    @ApiOkResponse({type: GetAllCitiesDTO, description: "List of all configured cities"})
     @Get("cities") 
     getListOfCities() {
         return this.weatherService.getAllCities();
     }
     
+
+    @ApiOkResponse({type: CreateCityDTO, description: "The name of the added city"})
     @ApiBasicAuth()
     @Post('add')
     @UsePipes(new ValidationPipe())
     addCity(@Body() city: CreateCityDTO) {
         this.weatherService.addCity(city)
-        return "City Added Successfully."
+        return city
     }
 
 }
