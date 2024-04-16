4cf884657ee131c32e6c2cdff676c36b41cffd31
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-16 13:00:25 +0530

    Update CreateCity.dto.ts

--- weather_api/src/weather/dtos/CreateCity.dto.ts
+++ weather_api/src/weather/dtos/CreateCity.dto.ts
@@ -5,12 +5,5 @@ export class CreateCityDTO {
     @ApiProperty({example:"Delhi", description: 'Name of the city' })
     @IsNotEmpty()
     city: string;
-
-    @ApiProperty({ description: 'Latitude of the city' })
-    @IsNotEmpty()
-    latitude: number;
-
-    @ApiProperty({ description: 'Longitude of the city' })
-    @IsNotEmpty()
-    longitude: number;
 }
+
