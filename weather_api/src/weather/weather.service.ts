8b19b7fac1e4127e94b24707c47b2b715bbb8b6f
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-16 12:58:26 +0530

    Update weather.service.ts

--- weather_api/src/weather/weather.service.ts
+++ weather_api/src/weather/weather.service.ts
@@ -6,6 +6,8 @@ import { CreateCityDTO } from "./dtos/CreateCity.dto";
 import { HttpService } from '@nestjs/axios';
 import { map, catchError, lastValueFrom } from 'rxjs';
 import { ConfigService } from "@nestjs/config";
+import { GetAllCitiesDTO } from "./dtos/GetAllCities.dto";
+import { GetWeatherDTO } from "./dtos/GetWeather.dto";
 
 @Injectable()
 export class WeatherService {
@@ -15,30 +17,42 @@ export class WeatherService {
     addCity(cityDTO: CreateCityDTO): Promise<City> {
         const model = new this.cityModel();
         model.city = cityDTO.city
-        model.latitude = cityDTO.latitude;
-        model.longitude = cityDTO.longitude
+        // model.latitude = cityDTO.latitude;
+        // model.longitude = cityDTO.longitude
         return model.save();
     }
 
-    async getAllCities(): Promise<string[]> {
+    async getAllCities(): Promise<GetAllCitiesDTO> {
         const all_cities_docs = await this.cityModel.find().exec();
-        return all_cities_docs.map(cityDoc => (cityDoc.city))
+        const allCitiesDto = new GetAllCitiesDTO()
+        allCitiesDto.cities = []
+        all_cities_docs.forEach(cityDoc =>  allCitiesDto.cities.push(cityDoc.city));
+        return allCitiesDto;
     }
+    
 
     async getWeather() {
         const all_cities_docs = await this.cityModel.find().exec();
         const apiKey = this.configService.get("API_KEY");
+
         const requests = all_cities_docs.map(async (cityDoc) => {
-            const lat = cityDoc.latitude;
-            const lon = cityDoc.longitude;
-            const apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+            const city = cityDoc.city;
+            // const lat = cityDoc.latitude;
+            // const lon = cityDoc.longitude;
+            const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
             const result = this.http.get(apiUrl).pipe(
-                map(response => ({
-                    city: cityDoc.city,
-                    weather: response.data['weather'][0]['description']
-                }))
+                map(response => {
+                    const getWeatherDto = new GetWeatherDTO()
+                    getWeatherDto.City = cityDoc.city;
+                    getWeatherDto.Maximum_Temperature = parseFloat((response.data['main']['temp'] - 273.15).toFixed(1)) + "°C";
+                    getWeatherDto.Minimum_Temperature = parseFloat((response.data['main']['temp_max'] - 273.15).toFixed(1)) + "°C";
+                    getWeatherDto.Temperature = parseFloat((response.data['main']['temp_min'] - 273.15).toFixed(1)) + "°C";
+                    return getWeatherDto
+                })
             );
-            return await lastValueFrom(result);
+            const data = await lastValueFrom(result);
+
+            return data
         });
 
         const results = await Promise.all(requests);
