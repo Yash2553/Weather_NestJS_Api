decb53424848f24599efe98bcd2d690975daaadf
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-19 10:41:25 +0530

    Update CreateCity.dto.ts

--- weather_api/src/weather/dtos/CreateCity.dto.ts
+++ weather_api/src/weather/dtos/CreateCity.dto.ts
@@ -1,9 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
 
 export class CreateCityDTO {
     @ApiProperty({example:"Delhi", description: 'Name of the city' })
-    @IsNotEmpty()
     city: string;
 }
 
