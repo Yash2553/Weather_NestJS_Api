ef5f1416d222dcab9d0c0bfe3a1f63c39c76ca57
Yash2553 <94919463+Yash2553@users.noreply.github.com>
2024-04-19 10:43:44 +0530

    Update weather.controller.ts

--- weather_api/src/weather/weather.controller.ts
+++ weather_api/src/weather/weather.controller.ts
@@ -1,23 +1,28 @@
 import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateCityDTO } from './dtos/CreateCity.dto';
 import { WeatherService } from 'src/weather/weather.service';
-import { ApiBasicAuth, ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { GetWeatherDTO } from './dtos/GetWeather.dto';
+import { ApiBadGatewayResponse, ApiBasicAuth, ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetAllCitiesDTO } from './dtos/GetAllCities.dto';
+import { GetWeatherDTO } from './dtos/GetWeather.dto';
+import { CityAlreadyExistsExceptions, InvalidCityInputException } from './exceptions/AddCity.exception';
+import { FailedToGetWeatherDataException } from './exceptions/GetWeather.exception';
 
 
 @ApiTags('weather')
 @Controller('weather')
 export class WeatherController {
     constructor (private readonly weatherService: WeatherService, ) {}
-    
-    
-    @ApiOkResponse({type:GetWeatherDTO, description:"Weather of all configured cities"})
+
+    @ApiResponse({status: 403, type: FailedToGetWeatherDataException, description: "Error Response when OpenWeather API calls fails for any reason"})
+    @ApiOkResponse({type: GetWeatherDTO, description: "Weather of all configured cities"})
     @Get()
-    getWeather() {
-        return this.weatherService.getWeather();
+    async getWeather() {
+        try {
+            return await this.weatherService.getWeather();
+        } catch (error){
+            throw error
+        }
     }
- 
 
     @ApiOkResponse({type: GetAllCitiesDTO, description: "List of all configured cities"})
     @Get("cities") 
@@ -26,13 +31,19 @@ export class WeatherController {
     }
     
 
+    @ApiResponse({status: 403, type: InvalidCityInputException, description: "Error response when city is not an valid input."})
+    @ApiResponse({status: 400, type: CityAlreadyExistsExceptions, description: "Error response when city is already present in the database."})
     @ApiOkResponse({type: CreateCityDTO, description: "The name of the added city"})
     @ApiBasicAuth()
     @Post('add')
     @UsePipes(new ValidationPipe())
-    addCity(@Body() city: CreateCityDTO) {
-        this.weatherService.addCity(city)
-        return city
+    async addCity(@Body() city: CreateCityDTO) {
+        try {
+            await this.weatherService.addCity(city);
+            return city;
+          } catch (error) {
+            throw error; 
+          }
     }
 
 }
